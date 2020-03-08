27c8c0c86efe5853d880fa34dacf7eab4e34d233
Andrew G. Crowell <overkill9999@gmail.com>
2020-03-07 19:37:22 -0500

    v2chr source

--- v2c15src/font.h
+++
@@ -1,7 +0,0 @@
-/* FONT.H header file for modified FONT.C font functions */
-
-extern unsigned char *fontset;
-extern void (*blitfont)(int x, int y, unsigned char *string,unsigned char c);
-extern void fontfuncs();
-
-/* end of file */
